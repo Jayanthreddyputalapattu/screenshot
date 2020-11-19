03aa6255bdbdb673c8754a968c19884c6f1a821e
Jayanth Reddy Putalapattu <74652746+Jayanthreddyputalapattu@users.noreply.github.com>
2020-11-19 13:51:52 +0530

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -7,7 +7,7 @@ root.geometry("500x500")
 
 def take_a_screenshot():
     screenshot = pyautogui.screenshot()
-    screenshot.save()
+    screenshot.save("screenshot.png")
 
 
 button = Button(root,text="Take a Screenshot",bg="yellow")
