415657676a5e0a55ae5180a728338fa272fffcc3
Jayanth Reddy Putalapattu <74652746+Jayanthreddyputalapattu@users.noreply.github.com>
2020-12-03 12:03:47 +0530

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,19 +1,17 @@
-from tkinter import *
 import pyautogui
+import tkinter as tk
 
+root= tk.Tk()
 
-root = Tk()
-root.geometry("500x500")
+canvas1 = tk.Canvas(root, width = 300, height = 300)
+canvas1.pack()
 
-def take_a_screenshot():
-    screenshot = pyautogui.screenshot()
-    screenshot.save("screenshot.png")
+def takeScreenshot ():
+    
+    myScreenshot = pyautogui.screenshot()
+    myScreenshot.save(r'C:\Users\Ron\Desktop\Test\screenshot2.png')
 
-
-button = Button(root,text="Take a Screenshot",bg="yellow",command=take_a_screenshot)
-button.pack()
-
-
-
+myButton = tk.Button(text='Take Screenshot', command=takeScreenshot, bg='green',fg='white',font= 10)
+canvas1.create_window(150, 150, window=myButton)
 
 root.mainloop()
