a500a8d98caa6eb844cdff7731d12ed770597b13
Jayanth Reddy Putalapattu <74652746+Jayanthreddyputalapattu@users.noreply.github.com>
2020-12-03 12:08:13 +0530

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -9,7 +9,7 @@ canvas1.pack()
 def takeScreenshot ():
     
     myScreenshot = pyautogui.screenshot()
-    myScreenshot.save(r'C:\Users\Ron\Desktop\Test\screenshot2.png')
+    myScreenshot.save("screenshot.png')
 
 myButton = tk.Button(text='Take Screenshot', command=takeScreenshot, bg='green',fg='white',font= 10)
 canvas1.create_window(150, 150, window=myButton)
